20af1cb8357b933f71b2e8d1c9a206b6df5ce049
Aram Harutyunyan <aramharutyunyan77@gmail.com@>
2023-01-17 20:11:37 +0400

    store is created, basic components folders are added

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+
+import './Layout.scss'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+const Layout = ({children}: LayoutProps) => {
+  return (
+    <div className='Layout'>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
